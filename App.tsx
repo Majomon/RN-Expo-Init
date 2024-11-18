658c82fc4ba65b17ac935f9681f126ff7fb47bb5
Mauri <mauri.monzon91@gmail.com>
2024-11-18 18:28:38 -0300

    Boton flotante personalizado

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
-import { Button, Pressable, StyleSheet, Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
+import FAB from "./components/FAB";
 
 export default function App() {
   const [count, setCount] = useState(10);
@@ -9,13 +10,12 @@ export default function App() {
       <Text>Hola mundo</Text>
       <Text style={styles.textHuge}>{count}</Text>
 
-      <Pressable
-        style={styles.floatingButton}
+      <FAB
+        label="+1"
         onPress={() => setCount(count + 1)}
         onLongPress={() => setCount(0)}
-      >
-        <Text style={{ color: "white", fontSize: 20 }}>+1</Text>
-      </Pressable>
+        position="right"
+      />
 
       <StatusBar style="auto" />
     </View>
